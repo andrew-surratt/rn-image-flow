0dc5eb9279f7a25d6a642fd607d41a3e74fddf32
Andrew Surratt <5783385+andrew-surratt@users.noreply.github.com>
2023-01-25 00:22:24 -0500

    Save camera photos to roll

--- src/route/visionResults.tsx
+++ src/route/visionResults.tsx
@@ -1,28 +1,115 @@
-import {GoogleCloudVisionResponse, ImageData} from '../types';
-import {Linking, View} from 'react-native';
-import {Button} from 'react-native-paper';
-import React, {useEffect, useState} from 'react';
+import {
+  GoogleCloudVisionError,
+  GoogleCloudVisionResponse,
+  ImageData,
+  isGoogleCloudVisionError,
+} from '../types';
+import { Linking, ScrollView, View } from 'react-native';
+import { ActivityIndicator, Button, List, Text } from 'react-native-paper';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles';
-import visionResultsPreview from '../component/visionResults';
 import { appStorage } from '../storage/appStorage';
-import {RootStackParamList} from './types';
-import {NativeStackNavigationProp} from '@react-navigation/native-stack/src/types';
+import { RootStackParamList } from './types';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack/src/types';
+import config from '../config/config';
+import { Adapter, AdapterType } from '../config/types';
+
+const VisionResultsPreview = (
+  visionResults?: GoogleCloudVisionResponse | GoogleCloudVisionError,
+  targetFlow?: Adapter
+) => {
+  const resultsList = isGoogleCloudVisionError(visionResults) ? (
+    <Text style={styles.visionResultsTextStyle}>
+      {JSON.stringify(visionResults?.error?.message)}
+    </Text>
+  ) : (
+    <List.AccordionGroup>
+      <List.Accordion titleStyle={styles.visionTextStyle} title={'Labels Found'} id={1}>
+        {visionResults?.responses[0]?.labelAnnotations?.map((label, key) => {
+          return (
+            <List.Item
+              key={`label${key}`}
+              title={label.description}
+              description={`${label.score?.toFixed(2)?.slice(2)}%`}
+              style={styles.visionListItemStyle}
+              titleStyle={styles.visionTextStyle}
+              descriptionStyle={styles.visionTextStyle}
+            />
+          );
+        })}
+      </List.Accordion>
+      <List.Accordion title={'Text Found'} titleStyle={styles.visionTextStyle} id={2}>
+        {visionResults?.responses[0]?.textAnnotations?.map((text, key) => (
+          <List.Item
+            key={`text${key}`}
+            title={text.description}
+            style={styles.visionListItemStyle}
+            titleStyle={styles.visionTextStyle}
+            descriptionStyle={styles.visionTextStyle}
+          />
+        ))}
+      </List.Accordion>
+    </List.AccordionGroup>
+  );
+  return (
+    <ScrollView style={styles.visionResultsStyle}>{resultsList}</ScrollView>
+  );
+};
 
 type Props = {
-  navigation: NativeStackNavigationProp<RootStackParamList, 'Vision Results'>
-}
+  navigation: NativeStackNavigationProp<RootStackParamList, 'Vision Results'>;
+};
 
-const VisionResults  = ({navigation}: Props) => {
-  const [visionResults, setVisionResults] = useState<GoogleCloudVisionResponse | null>(null);
-  const [imageData, setImageData] = useState<ImageData | null>(null);
+const calculateBestTargetFlow = (
+  visionResults?: GoogleCloudVisionResponse
+): Adapter | undefined => {
+  for (const flow of config.flows) {
+    const visionResponse = visionResults?.responses[0];
+    const containsLabel: boolean =
+      visionResponse?.labelAnnotations?.some(
+        (label) =>
+          label.description && flow.labelAnnotations.includes(label.description)
+      ) || false;
+    const containsText: boolean =
+      visionResponse?.textAnnotations?.some(
+        (text) =>
+          text.description && flow.textAnnotations.includes(text.description)
+      ) || false;
+    if (containsLabel || containsText) {
+      return config.adapters.find((adapter) => adapter.id === flow.action.id);
+    }
+  }
+  return undefined;
+};
+
+/**
+ * Get date formatted for google calendar.
+ * @param addMinutes
+ */
+const getGCalDate = (addMinutes = -30) => {
+  const now = new Date();
+  now.setMinutes(now.getMinutes() + addMinutes);
+  return `${now
+    .toISOString()
+    .slice(0, 19)
+    .replaceAll(':', '')
+    .replaceAll('-', '')}Z`;
+};
+
+const VisionResults = ({}: Props) => {
+  const [visionResults, setVisionResults] =
+    useState<GoogleCloudVisionResponse>();
+  const [imageData, setImageData] = useState<ImageData>();
+  const [targetFlow, setTargetFlow] = useState<Adapter>();
 
   useEffect(() => {
     async function getImageData() {
-      const storedResults = await appStorage.visionResults.get();
+      const storedResults = (await appStorage.visionResults.get()) || undefined;
       setVisionResults(storedResults);
-      const storedImageData = await appStorage.imageData.get();
+      const storedImageData = (await appStorage.imageData.get()) || undefined;
       setImageData(storedImageData);
       console.log(`Image data: ${JSON.stringify(storedImageData)}`);
+      setTargetFlow(calculateBestTargetFlow(storedResults));
     }
     getImageData();
   }, []);
@@ -31,42 +118,70 @@ const VisionResults  = ({navigation}: Props) => {
    * TODO: Open app based on vision results.
    */
   const openApp = () => {
-    (async() => {
-      if (visionResults?.responses) {
-        const title = 'Parking';
-        const description = visionResults?.responses[0]?.textAnnotations[0]?.description;
-        const location = imageData ? `${imageData.latitude}, ${imageData.longitude}` : '';
-        const getGCalDate = (addMinutes = -30) => {
-          const now = new Date();
-          now.setMinutes(now.getMinutes() + addMinutes);
-          return `${now.toISOString().slice(0, 19).replaceAll(':', '').replaceAll('-', '')}Z`;
-        };
-        const start = getGCalDate();
-        const end = getGCalDate(60);
-        const googleCalDeepLink = `https://www.google.com/calendar/render`;
-        const gCalActionParam = `action`;
-        const gCalCreateAction = `TEMPLATE`;
-        Linking.openURL(
-          `${googleCalDeepLink}?${gCalActionParam}=${gCalCreateAction}&text=${title}&details=${description}&location=${location}&dates=${start}%2F${end}`
-        );
+    (async () => {
+      if (visionResults?.responses && targetFlow) {
+        if (targetFlow) {
+          if (targetFlow.type === AdapterType.URL) {
+            // Todo: extract configuration
+            const variables: Record<string, string> = {
+              '1': 'Parking',
+              '2':
+                visionResults?.responses[0]?.textAnnotations[0]?.description ||
+                '',
+              '3': imageData
+                ? `${imageData.latitude}, ${imageData.longitude}`
+                : '',
+              '4': getGCalDate(),
+              '5': getGCalDate(60),
+            };
+            type AdapterUrlType = {
+              urlComponents: string[];
+            };
+            const variablePattern: RegExp = new RegExp(`^(\\\${)(\\w+)(})\$`);
+            const url = (targetFlow.value as AdapterUrlType).urlComponents
+              .map((component) => {
+                const regExpMatchArray = component.match(variablePattern);
+                console.log(`Matched component ${component}: ${regExpMatchArray}`);
+                if (regExpMatchArray && variables[regExpMatchArray[2]]) {
+                  return variables[regExpMatchArray[2]];
+                }
+                return component;
+              })
+              .join('');
+            console.log(`Linking to URL ${url}`);
+            Linking.openURL(url);
+          } else {
+            console.error(`No adapter for ${targetFlow.type}`);
+          }
+        } else {
+          console.error('No adapter configured.');
+        }
       } else {
-        console.error('Empty vision response');
+        console.error('No configured target flow.');
       }
     })();
   };
 
-  const startFlowEl: JSX.Element = visionResults ? (
-    <Button mode="contained" onPress={openApp} uppercase={false} style={[styles.visionResultsStyle, {justifyContent: 'center'}]}>
-      Start Configured Flow
-    </Button>
-  ) : (
-    <></>
-  );
+  const startFlowEl: JSX.Element =
+    visionResults && targetFlow ? (
+      <Button
+        mode="contained"
+        icon="tools"
+        onPress={openApp}
+        uppercase={false}
+        style={styles.visionResultsButtonStyle}
+        labelStyle={styles.imageButtonsLabelStyle}
+      >
+        {`Open ${targetFlow.name}`}
+      </Button>
+    ) : (
+      <ActivityIndicator />
+    );
 
   return (
     <View style={styles.contentStyle}>
+      {VisionResultsPreview(visionResults, targetFlow)}
       {startFlowEl}
-      {visionResultsPreview()}
     </View>
   );
 };
--- src/storage/types.ts
+++ src/storage/types.ts
@@ -1,14 +1,14 @@
-import {GoogleCloudVisionResponse, ImageData} from '../types';
+import { GoogleCloudVisionResponse, ImageData } from '../types';
 
 export interface StorageHandler<T> {
-  key: string,
-  get: () => Promise<T | null>,
-  set: (value: T) => Promise<void>,
-  clear: () => Promise<void>
+  key: string;
+  get: () => Promise<T | null>;
+  set: (value: T) => Promise<void>;
+  clear: () => Promise<void>;
 }
 
 export interface AppStorage {
-  imageUrl: StorageHandler<string>,
-  imageData: StorageHandler<ImageData>,
-  visionResults: StorageHandler<GoogleCloudVisionResponse>,
+  imageUrl: StorageHandler<string>;
+  imageData: StorageHandler<ImageData>;
+  visionResults: StorageHandler<GoogleCloudVisionResponse>;
 }
